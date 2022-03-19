34d281df61b42906cd6843ad7a413752668bbaa5
TheRealRondon <amirhossein2324236@gmail.com>
2022-03-19 14:13:41 +0330

    Updated create room component.

--- roompotify_backend/api/views.py
+++ roompotify_backend/api/views.py
@@ -11,9 +11,10 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+
 class CreateRoomView(APIView):
-    serializer_class = CreateRoomSerializer    
-    
+    serializer_class = CreateRoomSerializer
+
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
@@ -28,9 +29,12 @@ class CreateRoomView(APIView):
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
-                room.save(update_fileds=['guest_can_pause', 'votes_to_skip'])
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
                 room.save()
-            
-            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
