0afcde31e6f56d49e3ac970072720732cc80d337
TheRealRondon <amirhossein2324236@gmail.com>
2022-07-09 16:59:06 +0430

    Updated homepage.

--- roompotify_backend/frontend/src/components/HomePage.js
+++ roompotify_backend/frontend/src/components/HomePage.js
@@ -3,6 +3,7 @@ import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import HomePageUtil from "./HomePageUtil";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
 import {
   BrowserRouter as Router,
   Routes,
@@ -16,11 +17,33 @@ export default class HomePage extends Component {
     super(props);
   }
 
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h3" compact="h3">
+              Roompotify
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button color="primary" to="/join" component={ Link }>
+                Join a Room
+              </Button>
+              <Button color="secondary" to="/create" component={ Link }>
+                Create a Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+      </Grid>
+    );
+  }
+
   render() {
     return (
       <Router>
         <Routes>
-        <Route exact path="/" element={<HomePageUtil />} />
+        <Route exact path="/" element={this.renderHomePage()} />
           <Route exact path="/join" element={<RoomJoinPage />} />
           <Route exact path="/create" element={<CreateRoomPage />} />
           <Route exact path="/room/:roomCode" element={<Room />} />
