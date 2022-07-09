a12e0d700b8eb40688c900ead983e232a7bc720e
TheRealRondon <amirhossein2324236@gmail.com>
2022-07-09 17:36:23 +0430

    Added handling user sessions.

--- roompotify_backend/frontend/src/components/HomePage.js
+++ roompotify_backend/frontend/src/components/HomePage.js
@@ -10,31 +10,45 @@ import {
   Route,
   Link,
   Redirect,
+  Navigate,
 } from "react-router-dom";
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
+  }
+
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
   }
 
   renderHomePage() {
     return (
       <Grid container spacing={3}>
-          <Grid item xs={12} align="center">
-            <Typography variant="h3" compact="h3">
-              Roompotify
-            </Typography>
-          </Grid>
-          <Grid item xs={12} align="center">
-            <ButtonGroup disableElevation variant="contained" color="primary">
-              <Button color="primary" to="/join" component={ Link }>
-                Join a Room
-              </Button>
-              <Button color="secondary" to="/create" component={ Link }>
-                Create a Room
-              </Button>
-            </ButtonGroup>
-          </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            Roompotify
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
       </Grid>
     );
   }
@@ -43,7 +57,11 @@ export default class HomePage extends Component {
     return (
       <Router>
         <Routes>
-        <Route exact path="/" element={this.renderHomePage()} />
+          <Route exact path="/" element={
+            this.state.roomCode ?
+              <Navigate to={`/room/${this.state.roomCode}`} /> :
+              this.renderHomePage()
+          } />
           <Route exact path="/join" element={<RoomJoinPage />} />
           <Route exact path="/create" element={<CreateRoomPage />} />
           <Route exact path="/room/:roomCode" element={<Room />} />
