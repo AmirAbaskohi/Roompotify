12687d0afb30353716fb08ff68aa5be54ba5aa32
TheRealRondon <amirhossein2324236@gmail.com>
2022-07-09 18:36:28 +0430

    Updated handling leaving room.

--- roompotify_backend/frontend/src/components/Room.js
+++ roompotify_backend/frontend/src/components/Room.js
@@ -1,39 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
 
 export default class Room extends Component {
-    constructor(props) {
-        const location = window.location.href;
-        super(props);
-        this.state = {
-            votesToSkip: 2,
-            guestCanPause: false,
-            isHost: false
-        };
-        let splittedLocation = location.split("/");
-        this.roomCode = splittedLocation[splittedLocation.length - 1];
-        this.getRoomDetails();
-    }
+  constructor(props) {
+    const location = window.location.href;
+    super(props);
+    this.state = {
+      votesToSkip: 2,
+      guestCanPause: false,
+      isHost: false,
+    };
+    let splittedLocation = location.split("/");
+    this.roomCode = splittedLocation[splittedLocation.length - 1];
+    this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+  }
 
-    getRoomDetails() {
-        fetch('/api/get-room' + '?code=' + this.roomCode).then((response) =>
-            response.json()
-        ).then((data) => {
-            this.setState({
-                votesToSkip: data.votes_to_skip,
-                guestCanPause: data.guest_can_pause,
-                isHost: data.is_host,
-            });
+  getRoomDetails = () => {
+    return fetch("/api/get-room" + "?code=" + this.roomCode)
+      .then((response) => {
+        if (!response.ok) {
+
+          window.location.href = window.location.toString().split("/").slice(0, 3).join("/");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
         });
-    }
+      });
+  }
 
-    render() {
-        return ( 
-            <div>
-                <h3>{this.roomCode}</h3>
-                <p>Votes: {this.state.votesToSkip}</p>
-                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-                <p>Host: {this.state.isHost.toString()}</p>
-            </div>
-        );
-    }
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      window.location.href = window.location.toString().split("/").slice(0, 3).join("/");
+    });
+  }
+
+  render() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
